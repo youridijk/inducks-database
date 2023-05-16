ec8d5abd18bfb301c0614fd091fd319bc1a07fce
youridijk <steltje.maraboe_0q@icloud.com>
2023-05-16 21:15:26 +0200

    Fix auth script

--- docker/database/docker-entrypoint-initdb.d/postgrest/3-create-auth-user.sh
+++ docker/database/docker-entrypoint-initdb.d/postgrest/3-create-auth-user.sh
@@ -3,6 +3,6 @@
 FILE="/docker-entrypoint-initdb.d/4-create-auth-user.sql"
 if ! [[ -z $AUTH_USER || -z $AUTH_PASSWORD ]]; then
   echo "CREATE ROLE \"$AUTH_USER\";" > "$FILE"
-  echo "ALTER ROLE \"$AUTH_USER\" WITH password '$POSTGRES_AUTH_PASSWORD';" >> "$FILE"
+  echo "ALTER ROLE \"$AUTH_USER\" WITH password '$AUTH_PASSWORD' LOGIN;" >> "$FILE"
   echo "GRANT web_anon TO \"$AUTH_USER\";" >> "$FILE"
 fi
