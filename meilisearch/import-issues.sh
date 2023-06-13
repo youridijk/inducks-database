ada03d48115c7ab1ede09d47980a43809a55aba8
youridijk <steltje.maraboe_0q@icloud.com>
2023-06-13 12:26:31 +0200

    Add issue data for meilisearch

---
+++ meilisearch/import-issues.sh
@@ -0,0 +1,9 @@
+curl "https://inducks.dijk.cc/api/postgrest/meilisearch_issue" -o data/issues.json
+
+ curl \
+  -X POST 'https://meilisearch.dijk.cc/indexes/issues/documents' \
+  -H 'Content-Type: application/json' \
+  --data-binary @data/issues.json
+#https://inducks.org/hr.php?image=https%3A%2F%2Foutducks.org%2Fwebusers%2Fwebusers%2F2014%2F04%2Fnl_po3_219a_001.jpg&normalsize=1
+
+#https://outducks.org/webusers/webusers/2014/04/nl_po3_219a_001.jpg&normalsize=1
--- sql/meilisearch.sql
+++ sql/meilisearch.sql
@@ -1,34 +1,34 @@
-set search_path  to inducks;
+set search_path to inducks;
 DROP VIEW IF EXISTS meilisearch_character;
 CREATE OR REPLACE VIEW meilisearch_character AS
-SELECT MD5("inducks"."character"."charactercode")                      AS "id",
-       "inducks"."character"."charactercode",
-       "inducks"."character"."charactername",
-       "inducks"."character"."official",
-       "inducks"."character"."onetime",
-       "inducks"."character"."heroonly",
-       COALESCE("character_names"."character_names", '{}')             AS "character_names",
-       COALESCE("character_entry_names"."character_entry_names", '{}') AS "character_entry_names",
-       character_appearances                                           AS "character_appearances_count"
-FROM "inducks"."character"
-         LEFT JOIN LATERAL (
-    SELECT json_object_agg(charactername.languagecode, charactername.charactername) AS "character_names"
-    FROM "inducks"."charactername" AS "charactername"
-    WHERE "charactername"."charactercode" =
-          "inducks"."character"."charactercode") AS "character_names"
-                   ON TRUE
-         LEFT JOIN LATERAL (
-    SELECT array_agg(distinct "entrycharactername"."charactername") AS "character_entry_names"
-    FROM "inducks"."entrycharactername" AS "entrycharactername"
-    WHERE entrycharactername."charactercode" =
-          "inducks"."character"."charactercode") AS "character_entry_names"
+SELECT MD5(character.charactercode)                                AS id,
+       character.charactercode,
+       character.charactername,
+       character.official,
+       character.onetime,
+       character.heroonly,
+       COALESCE(character_names.character_names, '{}')             AS character_names,
+       COALESCE(character_entry_names.character_entry_names, '{}') AS character_entry_names,
+       character_appearances                                       AS character_appearances_count
+FROM character
+         LEFT JOIN LATERAL (SELECT json_object_agg(charactername.languagecode,
+                                                   charactername.charactername) AS character_names
+                            FROM charactername AS charactername
+                            WHERE charactername.charactercode =
+                                  character.charactercode) AS character_names
                    ON TRUE
 
-         LEFT JOIN LATERAL ( SELECT count(character_appearances) AS "character_appearances"
-                             FROM (SELECT "appearance"."charactercode"
-                                   FROM "inducks"."appearance" AS "appearance"
-                                   WHERE "appearance"."charactercode" =
-                                         "inducks"."character"."charactercode") AS "character_appearances" ) AS "character_appearances"
+         LEFT JOIN LATERAL (SELECT array_agg(distinct entrycharactername.charactername) AS character_entry_names
+                            FROM entrycharactername AS entrycharactername
+                            WHERE entrycharactername.charactercode =
+                                  character.charactercode) AS character_entry_names
+                   ON TRUE
+
+         LEFT JOIN LATERAL ( SELECT count(character_appearances) AS character_appearances
+                             FROM (SELECT appearance.charactercode
+                                   FROM appearance AS appearance
+                                   WHERE appearance.charactercode =
+                                         character.charactercode) AS character_appearances ) AS character_appearances
                    ON TRUE;
 
 GRANT SELECT ON meilisearch_character TO auth;
@@ -36,39 +36,77 @@ GRANT SELECT ON meilisearch_character TO auth;
 CREATE INDEX md5_charactercode ON character (charactercode);
 CREATE INDEX md5_issue ON issue (issuecode);
 
-DROP VIEW IF EXISTS "meilisearch_issue";
-CREATE OR REPLACE VIEW "meilisearch_issue" AS
-SELECT
-    MD5("inducks"."issue"."issuecode") AS "id",
-       "inducks"."issue"."issuecode",
-       "inducks"."issue"."issuerangecode",
-       "inducks"."issue"."publicationcode",
-       "inducks"."issue"."title",
-       "inducks"."issue"."issuenumber",
-       "inducks"."issue"."oldestdate",
-       "inducks"."issue"."filledoldestdate",
-       "inducks"."issue"."fullyindexed",
-       row_to_json("issue_publication_1".*) AS "publication",
-       CONCAT("issue_publication_1"."title", ' ', "inducks"."issue"."issuenumber", ' ',
-              "inducks"."issue"."title")  AS "full_title",
-       get_issue_image_urls("inducks"."issue"."issuecode") as "image_urls"
-FROM "inducks"."issue"
-         LEFT JOIN LATERAL ( SELECT "publication_1"."publicationcode",
-                                    "publication_1"."countrycode",
-                                    "publication_1"."languagecode",
-                                    "publication_1"."title"
-                             FROM "inducks"."publication" AS "publication_1"
-                             WHERE "publication_1"."publicationcode" =
-                                   "inducks"."issue"."publicationcode" ) AS "issue_publication_1" ON TRUE;
+DROP VIEW IF EXISTS meilisearch_issue;
+CREATE OR REPLACE VIEW meilisearch_issue AS
+SELECT MD5(i.issuecode)              AS id,
+       i.issuecode,
+       i.issuerangecode,
+       i.publicationcode,
+       i.title as issue_title,
+       i.issuenumber,
+       i.oldestdate,
+       i.filledoldestdate,
+       i.fullyindexed,
+       issue_publication.title as publicaton_title,
+       issue_publication.title || ' ' || i.issuenumber as publication_title_issue_number,
+--        row_to_json(issue_publication.*) AS publication,
+       CONCAT(issue_publication.title, ' ', i.issuenumber, ' ',
+              i.title)               AS full_title,
+       issue_publication.countrycode as countrycode,
+       issue_publication.languagecode as languagecode,
+--        entries
+        image_urls.*
+FROM issue i
+    LEFT JOIN LATERAL ( SELECT json_agg(i) as image_urls
+        FROM get_issue_image_urls(i.issuecode) i
 
-SELECT "inducks"."issue".*, row_to_json("issue_publication_1".*) AS "publication"
-FROM "inducks"."issue"
-         LEFT JOIN LATERAL ( SELECT "publication_1".*
-                             FROM "inducks"."publication" AS "publication_1"
-                             WHERE "publication_1"."publicationcode" =
-                                   "inducks"."issue"."publicationcode" ) AS "issue_publication_1" ON TRUE
+        ) AS image_urls ON TRUE
+
+         LEFT JOIN LATERAL ( SELECT p.publicationcode,
+                                    p.countrycode,
+                                    p.languagecode,
+                                    p.title
+                             FROM publication AS p
+                             WHERE p.publicationcode =
+                                   i.publicationcode ) AS issue_publication ON TRUE
+--          LEFT JOIN LATERAL (SELECT entry_urls
+--                             FROM entry e
+--                                      LEFT JOIN LATERAL (SELECT 'https://inducks.org/hr.php?image=' || site.urlbase ||
+--                                                                eu.url || '?normalsize=1' as fullurl
+--                                                         FROM entryurl eu
+--                                                                  LEFT JOIN LATERAL ( SELECT s.urlbase
+--                                                                                      FROM site s
+--                                                                                      WHERE s.sitecode = eu.sitecode
+--                                                             ) AS site ON TRUE
+--                                                         WHERE eu.entrycode = e.entrycode
+--                                                           AND eu.public = TRUE
+--                                 ) AS entry_urls ON TRUE
+--              where e.issuecode = i.issuecode
+--              and e.position = 'a'
+--     ) AS entries ON TRUE
+;
+
+
+
+SELECT issue.*, row_to_json(issue_publication_1.*) AS publication
+FROM issue
+         LEFT JOIN LATERAL ( SELECT publication_1.*
+                             FROM publication AS publication_1
+                             WHERE publication_1.publicationcode =
+                                   issue.publicationcode ) AS issue_publication_1 ON TRUE;
+
+
 GRANT SELECT ON meilisearch_issue TO web_anon;
 
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA inducks GRANT SELECT ON TABLES TO web_anon;
 
+select count(*)
+from meilisearch_issue
+
+select count(*)
+from character
+
+select issuenumber, title
+from issue
+where publicationcode = 'nl/PREM'
